fc9528aee17c08c22e5a365ffa129f4b1d3ba346
Alexander Shishenko <GamePad64@gmail.com>
2013-02-07 22:01:23 +0400

    Added messaging::TransportSocketMessageDispatcher stub Added relay::RelayTransportSocket stub

---
+++ src/daemon/messaging/TransportSocketMessageDispatcher.cpp
@@ -0,0 +1,38 @@
+/*
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "TransportSocketMessageDispatcher.h"
+
+namespace p2pnet {
+namespace messaging {
+
+TransportSocketMessageDispatcher::TransportSocketMessageDispatcher() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TransportSocketMessageDispatcher::~TransportSocketMessageDispatcher() {
+	// TODO Auto-generated destructor stub
+}
+
+void TransportSocketMessageDispatcher::receivedMessage(
+		net::MessageBundle message_bundle) {
+}
+
+void TransportSocketMessageDispatcher::sentMessage(
+		net::MessageBundle message_bundle) {
+}
+
+} /* namespace messaging */
+} /* namespace p2pnet */
