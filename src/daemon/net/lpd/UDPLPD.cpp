c61d2922aa50dba37f1708e3e403276d521e733f
Alexander Shishenko <GamePad64@gmail.com>
2013-03-01 00:00:37 +0400

    UDPLPDv4 now works

--- src/daemon/net/lpd/UDPLPD.cpp
+++ src/daemon/net/lpd/UDPLPD.cpp
@@ -14,6 +14,7 @@
 
 #include "UDPLPD.h"
 #include "../../AsioIOService.h"
+#include "../UDPTransportSocket.h"
 
 namespace p2pnet {
 namespace net {
@@ -29,24 +30,52 @@ UDPLPD::~UDPLPD() {
 }
 
 void UDPLPD::run() {
-	this->initSocket();
+	initSocket();
+	startReceive();
 	startSend();
 }
 
 UDPLPDMessage UDPLPD::generateMessage() {
 	UDPLPDMessage message;
-	message.set_port(m_target_port);
+	unsigned int my_port = m_config.getConfig().get("net.sockets.udpv4.port", 2185);
+	message.set_port(my_port);
 	return message;
+}
+
+void UDPLPD::processReceived(size_t bytes, std::shared_ptr<ip::udp::endpoint> endpoint, char* recv_buffer){
+	receive();
+	UDPLPDMessage message;
+	message.ParseFromString(std::string(recv_buffer, bytes));
+	std::clog << "[UDPLPD] Local <- " << endpoint->address().to_string() << ":" << message.port() << std::endl;
+	delete[] recv_buffer;
 }
 
 void UDPLPD::waitBeforeSend() {
 	m_timer.expires_from_now(boost::posix_time::seconds(m_timer_seconds));
-	m_timer.async_wait(boost::bind(&UDPLPD::startSend, this));
+	m_timer.async_wait(boost::bind(&UDPLPD::send, this));
+}
+
+void UDPLPD::send(){
+	std::clog << "[UDPLPD] Local -> " << m_target_address.to_string() << ":" << m_target_port << std::endl;
+	m_socket.async_send_to(buffer(udp_message), ip::udp::endpoint(m_target_address, m_target_port), boost::bind(&UDPLPD::waitBeforeSend, this));
+}
+
+void UDPLPD::receive(){
+	char* lpd_packet = new char[2048];
+	std::shared_ptr<ip::udp::endpoint> endpoint = std::make_shared<ip::udp::endpoint>(m_bind_address, m_target_port);
+	m_socket.async_receive_from(
+			buffer(lpd_packet, 2048), *endpoint,
+			boost::bind(&UDPLPD::processReceived, this, placeholders::bytes_transferred, endpoint, lpd_packet));
 }
 
 void UDPLPD::startSend() {
-	std::clog << "[UDPLPD] Local Peer Discovery message sent to: " << m_target_address.to_string() << " Port: " << m_target_port << std::endl;
-	m_socket.async_send_to(buffer(udp_message), ip::udp::endpoint(m_target_address, m_target_port), boost::bind(&UDPLPD::waitBeforeSend, this));
+	std::clog << "[UDPLPD] Started sending broadcasts to: " << m_target_address << ":" << m_target_port << std::endl;
+	send();
+}
+
+void UDPLPD::startReceive() {
+	std::clog << "[UDPLPD] Started receiving broadcasts from: " << m_target_address << ":" << m_target_port << std::endl;
+	receive();
 }
 
 } /* namespace lpd */
