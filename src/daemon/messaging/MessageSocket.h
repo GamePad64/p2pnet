33a43357bc8b8d58c7a1d39b88f6616b08e2ee4f
Alexander Shishenko <GamePad64@gmail.com>
2013-05-05 15:49:04 +0400

    Created stub for MessageSocket.

---
+++ src/daemon/messaging/MessageSocket.h
@@ -0,0 +1,33 @@
+/*
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MESSAGESOCKET_H_
+#define MESSAGESOCKET_H_
+
+namespace p2pnet {
+namespace messaging {
+
+class MessageSocket {
+	databases::PersonalKeyStorage* m_pks;
+	MessageGenerator m_generator;
+public:
+	MessageSocket();
+	virtual ~MessageSocket();
+
+
+};
+
+} /* namespace messaging */
+} /* namespace p2pnet */
+#endif /* MESSAGESOCKET_H_ */
