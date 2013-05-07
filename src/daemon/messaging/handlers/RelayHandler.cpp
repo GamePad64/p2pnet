ce20c2a9feaa5b2aa4af7faeb06ef13f5132585d
Alexander Shishenko <GamePad64@gmail.com>
2013-05-07 17:11:12 +0400

    Added RelayHandler stub.

---
+++ src/daemon/messaging/handlers/RelayHandler.cpp
@@ -0,0 +1,40 @@
+/*
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "RelayHandler.h"
+#include <iostream>
+
+namespace p2pnet {
+namespace messaging {
+namespace handlers {
+
+RelayHandler::RelayHandler(MessageSocket* socket_ptr) : MessageHandler(socket_ptr) {}
+RelayHandler::~RelayHandler() {}
+
+void RelayHandler::processReceivedMessage(protocol::p2pMessage& message, MessageProps& message_props) {
+	if (message.header().has_dest_th()
+			|| message.header().dest_th() != m_pks->getMyTransportHash().toBinaryString()){
+		// TODO Relay.
+		std::clog << "[RelayHandler] Relay message received. Not implemented yet."
+		message_props.skip = true;
+	}
+}
+
+void RelayHandler::processSentMessage(protocol::p2pMessage& message, MessageProps& message_props) {
+
+}
+
+} /* namespace handlers */
+} /* namespace messaging */
+} /* namespace p2pnet */
---
+++ src/daemon/messaging/handlers/RelayHandler.h
@@ -0,0 +1,36 @@
+/*
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RELAYHANDLER_H_
+#define RELAYHANDLER_H_
+
+#include "MessageHandler.h"
+
+namespace p2pnet {
+namespace messaging {
+namespace handlers {
+
+class RelayHandler : MessageHandler {
+public:
+	RelayHandler(MessageSocket* socket_ptr);
+	virtual ~RelayHandler();
+
+	void processReceivedMessage(protocol::p2pMessage& message, MessageProps& message_props);
+	void processSentMessage(protocol::p2pMessage& message, MessageProps& message_props);
+};
+
+} /* namespace handlers */
+} /* namespace messaging */
+} /* namespace p2pnet */
+#endif /* RELAYHANDLER_H_ */
