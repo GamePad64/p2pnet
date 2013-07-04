4b105e34523019e4a1908da1b88cdd3d20226d36
Alexander Shishenko <GamePad64@gmail.com>
2013-07-04 17:07:56 +0400

    Rewrited UDPLPD to work with TransportSocketInterfaces.

--- src/daemon/net/lpd/UDPLPDv4.cpp
+++ src/daemon/net/lpd/UDPLPDv4.cpp
@@ -13,13 +13,12 @@
  */
 
 #include "UDPLPDv4.h"
-#include "../udp/UDPTransportSocketEndpoint.h"
 
 namespace p2pnet {
 namespace net {
 namespace lpd {
 
-UDPLPDv4::UDPLPDv4(Config& config, net::UDPTransportSocket& udp_socket) : UDPLPD(config, udp_socket) {
+UDPLPDv4::UDPLPDv4(Config& config) : UDPLPD(config) {
 	readConfig();
 	initSocket();
 }
